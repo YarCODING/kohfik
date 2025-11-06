934676f342f9dc7631316b8dfefc4617ecfa10d1
Yaroslav <20011aric@gmail.com>
2025-11-06 15:22:34 +0200

    add

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+ 
+
+site = Flask(__name__)
+
+@site.route("/", methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@site.route("/about", methods=['GET'])
+def about():
+    return render_template("about.html")
+
+@site.route("/menu", methods=['GET'])
+def menu():
+    return render_template("menu.html")
+
+@site.route("/blog", methods=['GET'])
+def blog():
+    return render_template("blog.html")
+
+@site.route("/contact", methods=['GET'])
+def contact():
+    return render_template("contact.html")
+
+
+site.run(host='localhost', port=8000)
